98d29dfee05a251960206165342792c8c96fff5d
faeljedd <faeljedd@student.42.fr>
2026-02-10 11:13:02 -0700

    test

--- py_02/ex3/ft_finally_block.py
+++ py_02/ex3/ft_finally_block.py
@@ -1,29 +1,40 @@
 
 def water_plants(plant_list):
-    success = False
+
+    if not plant_list:
+        raise ValueError ("Cannot water None - invalid plant!")
+    for plant in plant_list:
+        if not plant:
+            raise ValueError ("Cannot water None - invalid plant!")
+        print(f"Watring {plant}")
+
+
+def test_watring_system():
+    success: bool = False
+    print("=== Garden Watering System ===\n")
     try:
-        print("Opening watering system")
-        for plant in plant_list:
-            if plant is None:
-                raise ValueError("Cannot water None - invalid plant!")
-            print(f"watering {plant}")
-        success = True
+        print("Testing normal watering...")
+        good_plans = ["", "lettuce", "carrots"]
+        water_plants(good_plans)
+
     except ValueError as e:
         print(f"Error: {e}")
-
+    else:
+        success = True
     finally:
         print("Closing watering system (cleanup)")
-    if success:
+    if success is True:
         print("Watering completed successfully!\n")
 
+    try:
+        print("Testing with error...")
+        bad_plants = ["tomato", None, "carrots"]
+        water_plants(bad_plants)
 
-def test_watering_system():
-    print("=== Garden Watering System ===\n")
-    print("Testing normal watering...")
-    good_plant = ["tomato", "lettuce", "carrots"]
-    water_plants(good_plant)
+    except ValueError as e:
+        print(f"Error: {e}")
+    finally:
+        print("Closing watering system (cleanup)\n")
+    print("Cleanup always happens, even with errors!")
 
-    print("Testing with error...")
-    bad_plants = ["tomato", None, "carrots"]
-    water_plants(bad_plants)
-    print("\nCleanup always happens, even with errors!")
+test_watring_system()
